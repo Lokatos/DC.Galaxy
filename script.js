4d8f254740fc63827d85482cfbf1ae32a13f3344
Roberto Jefferson <jeff_xd00@hotmail.com>
2025-04-20 20:02:48 -0300

    2° commit

---
+++ script.js
@@ -0,0 +1,8 @@
+<script>
+    $(document).ready(function(){
+        $('.botao-menu button').click(function(){
+            console.log('Clique funcionando!');
+            $(".menu-area").slideToggle();
+        });
+    });
+</script>
