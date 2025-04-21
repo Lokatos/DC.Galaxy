874a5b72f0f6e78df4f1d471b91006b649511314
Roberto Jefferson <jeff_xd00@hotmail.com>
2025-04-20 22:34:04 -0300

    3°

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-<script>
     $(document).ready(function(){
         $('.botao-menu button').click(function(){
             console.log('Clique funcionando!');
             $(".menu-area").slideToggle();
         });
     });
-</script>
